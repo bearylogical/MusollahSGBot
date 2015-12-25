7e46f4e8a98f2d974ef50e0de49504bab7d8afff
Syamil <syamil8@gmail.com>
2015-12-25 09:29:51 +0800

    cleanup index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var TelegramBot = require('node-telegram-bot-api');
 var CREDENTIALS = require('./private/telegram_credentials.json');
 var chalk = require('chalk');
 
+var locator = require('./public/api/locator')
 
 var bot = new TelegramBot(CREDENTIALS.token, {
     polling: true
@@ -20,6 +21,9 @@ bot.on('message', function(msg) {
         if (!msg.hasOwnProperty('text') && !msg.hasOwnProperty('location')) {
             return false;
         }
+        if (msg.hasOwnProperty('location')) {
+            return locator.processLocation(msg);
+        }
         var chatId = msg.chat.id;
         var body = msg.text;
         var command = body;
@@ -32,14 +36,28 @@ bot.on('message', function(msg) {
         switch (command.toLowerCase()) {
             case "start":
                 return help(chatId);
+            case "musollah":
+                return locator.musollahLocator(chatId);
+        }
+        switch (body.toLowerCase()) {
+            default:
+                var musollahSession = musollahSessions[chatId] || new musollahSession(chatId);
+                if (musollahSession.onGoing) {
+                    return locator.musollahLocator(chatId, body.toLowerCase(), msg.location);
+                }
         }
         return default_msg(chatId);
     } catch (e) {
-        bot.sendMessage(msg.chat.id, "Cinnabot is sleeping right now 😴 Wake him up later.").then(function() {
-            bot.sendMessage(msg.chat.id, "Here's a catfact instead:").then(function() {
-                catfact(msg.chat.id);
-            });
-        });
+        bot.sendMessage(msg.chat.id, "MusollahBot has encountered an Error, please try again later");
         bot.sendMessage('49892469', e.toString());
     }
 });
+
+function default_msg(chatId) {
+    bot.sendMessage(chatId, "Sorry, I don't understand you! Try another command instead", {
+        reply_markup: JSON.stringify({
+            hide_keyboard: true
+        })
+    });
+}
+
