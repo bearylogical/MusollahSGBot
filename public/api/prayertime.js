ecd532aef71f3f51101bbaf46b8c07e86586f636
Syamil <syamil8@gmail.com>
2016-01-02 16:45:13 +0800

    fixed date issue with prayer

--- public/api/prayertime.js
+++ public/api/prayertime.js
@@ -7,12 +7,10 @@ function sendPrayerTime(chatId,bot) {
     var today = new Date().toString().substr(0, 16);
     var message = String();
     message = "Prayer Times For" + '\n';
-
     var date = new Date();
-    if (date.getFullYear() == 2016) {
+    if (date.getFullYear() === 2016) {
 
-        var targetDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-
+        var targetDate = date.toDateString().split(' ')[2] + "/" + ("0" + (date.getMonth() + 1)).slice(-2) + "/" + date.getFullYear();
         for (var i = 0; i < prayertimes.events.length; i++) {
             if (targetDate == prayertimes.events[i].Date) {
                 message = message + "_"+ prayertimes.events[i].Hijri +"_" + " *|* " + today + '\n\n';
@@ -22,6 +20,7 @@ function sendPrayerTime(chatId,bot) {
                 message = message + "*Asar* " + prayertimes.events[i].Asar.replace(" ", ":") + "pm" + '\n';
                 message = message + "*Maghrib* " + prayertimes.events[i].Maghrib.replace(" ", ":") + "pm" + '\t';
                 message = message + "*Isyak* " + prayertimes.events[i].Isyak.replace(" ", ":") + "pm" + '\n';
+
                 bot.sendMessage(chatId, message,{
                     parse_mode : "Markdown"
                 });
